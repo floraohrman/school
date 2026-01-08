d63b4f69006ffad4ef522099604f6cb296062d71
floraohrman <floraohrman@gmail.com>
2026-01-08 19:54:46 +0000

    nivå 3 klar

--- javascript 2/Dag 1 övnigar/nivå 3/script.js
+++ javascript 2/Dag 1 övnigar/nivå 3/script.js
@@ -13,4 +13,27 @@ input.type = "number";
 input.placeholder = "Ange min ålder";
 
 const button = document.createElement("button");
-button.textContent = "Visa namn";
+button.textContent = "Filtrera";
+
+const resulDiv = document.createElement("div");
+
+document.body.appendChild(input);
+document.body.appendChild(button);
+document.body.appendChild(resulDiv);
+
+// Logik
+function filterByAge() {
+  const minAge = Number(input.value);
+  resulDiv.innerHTML = "";
+
+  for (let i = 0; i < people.length; i++) {
+        if (people[i].age > minAge) {
+        const p = document.createElement("p");
+        p.textContent = `${people[i].name}, ${people[i].age} år`;
+        resulDiv.appendChild(p);      
+        }
+    }
+}
+
+// Eventlyssnare
+button.addEventListener("click", filterByAge);
